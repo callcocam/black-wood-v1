d6dbca3fa5214663c55f8c4d3f906e82cf1cb58d
callcocam <callcocam@gmail.com>
2022-02-22 11:33:36 -0300

    carrinho

--- app/Http/Livewire/Paginas/Traits/CartTrait.php
+++ app/Http/Livewire/Paginas/Traits/CartTrait.php
@@ -8,12 +8,31 @@ trait CartTrait
     
     public $quantidade = 1;
 
-    public function add($produto)
+    public function add($produto,$item)
     {
-        if($item = $this->current_order()->items()->where('produto_id', $produto)->first()){
-            $item->update([
-                'total'=>Calcular($item->quantidade, $item->sale_price, '*',false),
-            ]);
+       
+        $item->update([
+            'total'=>Calcular($item->quantidade, $item->sale_price, '*',false),
+        ]);
+        $this->load_items();
+    }
+
+    public function up($produto)
+    {
+        if($current_order = $this->current_order()){
+            if($item = $current_order->items()->where('produto_id', $produto)->first()){
+                $item->increment('quantidade', 1);
+                $this->add($produto,$item);
+            }
+            else{
+                $produto = \App\Models\Produto::find($produto);
+                    $this->current_order()->items()->create([
+                    'produto_id'=>$produto->id,
+                    'quantidade'=>$this->quantidade,
+                    'sale_price'=> $produto->sale_price,
+                    'total'=> $produto->sale_price,
+                ]);
+            }
         }
         else{
             $produto = \App\Models\Produto::find($produto);
@@ -24,7 +43,27 @@ trait CartTrait
                 'total'=> $produto->sale_price,
             ]);
         }
-    
+        //$this->quantidade++;
+    }
+
+    public function down($produto)
+    {
+        if($current_order = $this->current_order()){
+            if($item = $current_order->items()->where('produto_id', $produto)->first()){
+                if($item->quantidade == 1){
+                    $item->delete();  
+                    $this->load_items();
+                }    
+                else{
+                    $item->decrement('quantidade', 1);   
+                    $this->add($produto,$item);
+                }
+            }                  
+            
+        }
+    }
+    public function load_items()
+    {
         $pedido = \DB::table('pedido_items')
         ->select(\DB::raw('sum(total) as total_pedido'))
         ->where('pedido_id',  $this->current_order()->id)
@@ -35,23 +74,6 @@ trait CartTrait
             'total'=>$pedido->total_pedido
         ]);
         $this->emit('cartUpdate');
-    }
-
-    public function up($produto)
-    {
-        if($item = $this->current_order()->items()->where('produto_id', $produto)->first()){
-            $item->increment('quantidade', 1);
-        }
-        //$this->quantidade++;
-        $this->add($produto);
-    }
-
-    public function down($produto)
-    {
-        if($item = $this->current_order()->items()->where('produto_id', $produto)->first()){
-            $item->decrement('quantidade', 1);
-        }
-        $this->add($produto);
     }
 
     public function item()
